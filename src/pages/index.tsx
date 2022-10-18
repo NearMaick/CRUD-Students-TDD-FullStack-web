7d4ae49b7f4365479e7920409cbaeb959c49969f
Maick Souza <maick_a_s@msn.com>
2022-10-18 14:12:18 -0300

    style(web): setting basic css #2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,16 @@ import styled from "./styles/home.module.css";
 export default function Home() {
   return (
     <div className={styled.container}>
-      <h1 className={styled.title}>Maick Souza</h1>
+      <h1 className={styled.title}>Cadastrar um estudante</h1>
+      <input
+        className={styled.input}
+        type="text"
+        placeholder="Digite o nome aqui"
+      />
+      <div className={styled.content}>
+        <p>Maick Souza</p>
+        <p>Enilda Maria</p>
+      </div>
     </div>
   );
 }

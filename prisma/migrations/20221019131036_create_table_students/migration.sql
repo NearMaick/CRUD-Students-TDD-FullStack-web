103c0b1e86359e8fc8d0eed4e50b9d572a4e1182
Maick Souza <maick_a_s@msn.com>
2022-10-19 10:11:44 -0300

    chore(server): setting up prisma backend server #2

---
+++ prisma/migrations/20221019131036_create_table_students/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_email_key" ON "Student"("email");

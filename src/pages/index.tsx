79ac144def8eae38f084da562b39e2815d4125eb
Maick Souza <maick_a_s@msn.com>
2022-10-19 09:10:16 -0300

    feat(web): making a button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import styled from "./styles/home.module.css";
 
 export default function Home() {
+  const [name, setName] = useState("");
+
+  function handleSendName() {
+    console.log(name);
+  }
+
   return (
     <div className={styled.container}>
       <h1 className={styled.title}>Cadastrar um estudante</h1>
       <input
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+        value={name}
         className={styled.input}
         type="text"
         placeholder="Digite o nome aqui"
       />
+      <button onClick={handleSendName} className={styled.button}>
+        Enviar
+      </button>
       <div className={styled.content}>
         <p>Maick Souza</p>
         <p>Enilda Maria</p>
